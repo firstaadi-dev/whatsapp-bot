f8e3748aaafdd68eddda0e48e6763ebd932f641e
Firsta Adi Pradana <firsta.pradana@majoo.id>
2024-02-25 13:34:55 +0700

    qr

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Client, LocalAuth } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
 
 const client = new Client({
   restartOnAuthFail: true,
@@ -10,7 +11,8 @@ const client = new Client({
 });
 
 client.on("qr", (qr) => {
-  console.log("QR RECEIVED", qr);
+  console.log("QR RECEIVED");
+  qrcode.generate(qr, { small: true });
 });
 
 client.on("ready", () => {
